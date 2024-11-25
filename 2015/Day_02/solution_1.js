874052fc90707361c5692d69083a0a0570f92f76
JamalColaire <jamal8colaire@gmail.com>
2024-11-25 17:19:42 -0500

    getting to solution

--- 2015/Day_02/solution_1.js
+++ 2015/Day_02/solution_1.js
@@ -3,15 +3,19 @@ Day 2 Puzzle-1 solution
 
 
 */
+ 
+let l = 0 // length
+let w = 0 // width
+let h = 0 // height
 
 function squareFt(x){
   for(i = 0; i < x.length; i++){
 
-    let arr = i.split('') // Making an array out of the elements of the input
+    let arr = i.split('x') // Making an array out of the elements of the input split by x
 
-    for(let j of arr){ // iterating the new array
+    for(let j of arr){ // iterating through the new array
       if(j >= 0){
-        let l = 
+         
       }
     }
   }
@@ -19,6 +23,6 @@ function squareFt(x){
 
 let input = ""
 
-let ans = input.split(" ")
+let ans = input.split(' ')
 
 console.log(squareFt(ans))
