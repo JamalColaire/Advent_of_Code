d6d09bf7b2f6f9fbac17ffd4dc30bf5bf4b11eda
JamalColaire <jamal8colaire@gmail.com>
2024-11-25 16:54:56 -0500

    Working on day 2

--- 2015/Day_02/solution_1.js
+++ 2015/Day_02/solution_1.js
@@ -4,4 +4,21 @@ Day 2 Puzzle-1 solution
 
 */
 
-console.log("Testing 2")
+function squareFt(x){
+  for(i = 0; i < x.length; i++){
+
+    let arr = i.split('') // Making an array out of the elements of the input
+
+    for(let j of arr){ // iterating the new array
+      if(j >= 0){
+        let l = 
+      }
+    }
+  }
+}
+
+let input = ""
+
+let ans = input.split(" ")
+
+console.log(squareFt(ans))
