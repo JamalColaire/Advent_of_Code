746214cf0b23405e79a8e0d2d7837b3434949558
JamalColaire <jamal8colaire@gmail.com>
2024-11-26 08:11:52 -0500

    solution: works on the first time

--- 2015/Day_02/solution_1.js
+++ 2015/Day_02/solution_1.js
@@ -3,26 +3,25 @@ Day 2 Puzzle-1 solution
 
 
 */
- 
-let l = 0 // length
-let w = 0 // width
-let h = 0 // height
 
 function squareFt(x){
+  let total = 0 // total combination of all the squareroots in the input
+
   for(i = 0; i < x.length; i++){
 
-    let arr = x[i].split('x') // spliting the numbers in the string by x and making an array 
+    let arr = x[i].split('x') // splitting the numbers in the string by x and making an array 
 
     for(let j of arr){ // iterating through the new array and converting the string elements into numbers
       Number(j) 
     }
+    total += (arr[0]*2 + arr[1]*2 + arr[2]*2) // squareroot formula
   }
 
-  return(arr[0]*2 +arr[1]*2 + arr[2]*2) //squareroot
+  return total 
 }
 
-let input = ""
+let input = "4x4x4 4x4x4" // sample input
 
-let ans = input.split(' ')
+let ans = input.split(' ') // splitting the input by spaces and returning an array
 
 console.log(squareFt(ans)) //solution
