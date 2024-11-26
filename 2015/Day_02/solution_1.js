6a609c130178a064d7e554d7ce9f76a3cfb9d586
JamalColaire <jamal8colaire@gmail.com>
2024-11-25 19:51:40 -0500

    solved maybe?

--- 2015/Day_02/solution_1.js
+++ 2015/Day_02/solution_1.js
@@ -11,18 +11,18 @@ let h = 0 // height
 function squareFt(x){
   for(i = 0; i < x.length; i++){
 
-    let arr = i.split('x') // Making an array out of the elements of the input split by x
+    let arr = x[i].split('x') // spliting the numbers in the string by x and making an array 
 
-    for(let j of arr){ // iterating through the new array
-      if(j >= 0){
-         
-      }
+    for(let j of arr){ // iterating through the new array and converting the string elements into numbers
+      Number(j) 
     }
   }
+
+  return(arr[0]*2 +arr[1]*2 + arr[2]*2) //squareroot
 }
 
 let input = ""
 
 let ans = input.split(' ')
 
-console.log(squareFt(ans))
+console.log(squareFt(ans)) //solution
