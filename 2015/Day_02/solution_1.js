3a861880d51dd9ae76bc88323dfc138b500fec50
JamalColaire <jamal8colaire@gmail.com>
2024-11-27 13:29:21 -0500

    solution: foundation complete

--- 2015/Day_02/solution_1.js
+++ 2015/Day_02/solution_1.js
@@ -14,13 +14,28 @@ function squareFt(x){
     for(let j of arr){ // iterating through the new array and converting the string elements into numbers
       Number(j) 
     }
-    total += (arr[0]*2 + arr[1]*2 + arr[2]*2) // squareroot formula
+
+    let side1 = arr[0]*arr[1] //lxw
+    let side2 = arr[1]*arr[2] //wxh
+    let side3 = arr[2]*arr[0] //hxl
+    
+    let smallSide = 0 //smallest side
+
+    if(side1 < side2 && side3){
+      smallSide = side1
+    }else if(side2 < side1 && side3){
+      smallSide = side2
+    }else if(side3 < side1 && side2){
+      smallSide = side3
+    }
+
+    total += ((arr[0]*arr[1]*2) + (arr[1]*arr[2]*2) + (arr[2]*arr[0]*2) + smallSide) // squareroot formula (2xlxw + 2xwxh + 2xhxl) + area of smallest side
   }
 
   return total 
 }
 
-let input = "4x4x4 4x4x4" // sample input
+let input = "1x1x10" // sample input
 
 let ans = input.split(' ') // splitting the input by spaces and returning an array
 
