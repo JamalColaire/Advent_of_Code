db1e974a9825ad8e2f7a69cff8a253d971bc7f50
JamalColaire <jamal8colaire@gmail.com>
2024-12-01 12:39:34 -0500

    Aoc prep

--- 2024/Day_02/solution_1.js
+++ 2024/Day_02/solution_1.js
@@ -4,10 +4,13 @@ Day 1 Puzzle-1 solution
 
 */
 
+function sol1(x){
 
+  for(let e of x){}
+}
 
 let input = ""
 
 let ans = input
 
-console.log() //solution
+console.log(sol1(ans)) //solution
--- 2024/Day_02/solution_2.js
+++ 2024/Day_02/solution_2.js
@@ -4,10 +4,13 @@ Day 1 Puzzle-2 solution
 
 */
 
+function sol2(x){
 
+  for(let e of x){}
+}
 
 let input = ""
 
 let ans = input
 
-console.log() //solution
+console.log(sol2(ans)) //solution
