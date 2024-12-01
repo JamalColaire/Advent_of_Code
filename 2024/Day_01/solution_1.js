af01e86876140036387dee46a0f1886fbe31783d
JamalColaire <jamal8colaire@gmail.com>
2024-12-01 11:43:01 -0500

    partial first solution

--- 2024/Day_01/solution_1.js
+++ 2024/Day_01/solution_1.js
@@ -4,14 +4,47 @@ Day 1 Puzzle-1 solution
 
 */
 
-function sol1(x){
-  for(let e of x){
+function listDiff(x){
 
+  let lArr = []
+  let rArr = []
+  
+  for(let e of x){
+    e.split('   ')
   }
+
+  for(let i = 0; i < x.length; i++){
+    for(let j = 0; j < x[i].length; j++){
+      if(j == 0){
+      lArr = lArr.push(x[i][j])
+      }else(rArr = rArr.push(x[i][j]))
+    }
+  }
+
+  lArr.sort()
+  rArr.sort()
+
+  let total = 0
+
+  for(let i = 0; i < lArr.length; i++){
+    if(lArr[i] > rArr[i]){
+      let diff = lArr[i] - rArr[i]
+    }else if(rArr[i] > lArr[i]){
+      let diff = rArr[i] - lArr[i]
+    }
+    total += diff
+  }
+
+  return total
 }
 
-let input = ""
+let input = "3   4
+4   3
+2   5
+1   3
+3   9
+3   3";
 
-let ans = input.split('')
+let ans = input.split('\n')
 
-console.log(sol1(ans)) //solution
+console.log(listDiff(ans)) //solution
