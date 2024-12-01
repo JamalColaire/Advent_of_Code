f1fbebccf5e26db512e20a424888d7949bb209da
JamalColaire <jamal8colaire@gmail.com>
2024-12-01 12:23:31 -0500

    partial solution

--- 2024/Day_01/solution_2.js
+++ 2024/Day_01/solution_2.js
@@ -5,13 +5,48 @@ Day 1 Puzzle-2 solution
 */
 
 function sol2(x){
-  for(let e of x){
 
+  for(let e of x){
+    e.split('   ')
   }
+
+  let lArr = []
+  let rArr = []
+
+  for(let i = 0; i < x.length; i++){
+    for(let j = 0; j < x[i].length; j++){
+      if(j == 0){
+      lArr = lArr.push(x[i][j])
+      }else(rArr = rArr.push(x[i][j]))
+    }
+  }
+
+  for(let i = 0; i < lArr.length; i++){
+    Number(lArr[i])
+    Number(rArr[i])
+  }
+
+  let total = 0
+  let multi = 0
+
+  for(let i = 0; i < lArr.length; i++){
+    for(let j = o; j < rArr.length; j++){
+      if(lArr[i] == rArr[j]){
+        multi++
+      }
+      lArr[i] = lArr[i] * multi
+    }
+  }
+
+  for(let el of lArr){
+    total += el
+  }
+
+  return total
 }
 
 let input = ""
 
-let ans = input.split('')
+let ans = input.split('\n')
 
 console.log(sol2(ans)) //solution
