ab88275a5af683f4cb19e320923062b50b32f596
JamalColaire <jamal8colaire@gmail.com>
2024-12-01 20:00:27 -0500

    added comments

--- 2024/Day_01/solution_1.js
+++ 2024/Day_01/solution_1.js
@@ -6,14 +6,14 @@ Day 1 Puzzle-1 solution
 
 function listDiff(x){
 
-  for(let e of x){
+  for(let e of x){ // Splitting the elements of the input to accuire the two numbers
     e.split('   ')
   }
 
-  let lArr = []
-  let rArr = []
+  let lArr = [] // left Array
+  let rArr = [] // Right Array
 
-  for(let i = 0; i < x.length; i++){
+  for(let i = 0; i < x.length; i++){ // Inserting the numbers into the appropriate array side
     for(let j = 0; j < x[i].length; j++){
       if(j == 0){
       lArr = lArr.push(x[i][j])
@@ -21,18 +21,18 @@ function listDiff(x){
     }
   }
 
-  for(let i = 0; i < lArr.length; i++){
+  for(let i = 0; i < lArr.length; i++){ // converting the strings into numbers
     Number(lArr[i])
     Number(rArr[i])
   }
 
-  lArr.sort()
+  lArr.sort() // ordering the numbers in the new array
   rArr.sort()
 
-  let total = 0
+  let total = 0 // summ of the differences
   let diff = 0
 
-  for(let i = 0; i < lArr.length; i++){
+  for(let i = 0; i < lArr.length; i++){ // comparing the two equivalent indexes based on size
     if(lArr[i] > rArr[i]){
       diff = lArr[i] - rArr[i]
     }else if(rArr[i] > lArr[i]){
