f2a3884621e77de7e08f17650013a0cb422d9570
JamalColaire <jamal8colaire@gmail.com>
2024-12-01 20:00:52 -0500

    added comments

--- 2024/Day_01/solution_2.js
+++ 2024/Day_01/solution_2.js
@@ -6,14 +6,14 @@ Day 1 Puzzle-2 solution
 
 function sol2(x){
 
-  for(let e of x){
+  for(let e of x){ // Splitting the elements of the input to accuire the two numbers
     e.split('   ')
   }
 
-  let lArr = []
-  let rArr = []
+  let lArr = [] // left Array
+  let rArr = [] // Right Array
 
-  for(let i = 0; i < x.length; i++){
+  for(let i = 0; i < x.length; i++){ // Inserting the numbers into the appropriate array side
     for(let j = 0; j < x[i].length; j++){
       if(j == 0){
       lArr = lArr.push(x[i][j])
@@ -21,24 +21,24 @@ function sol2(x){
     }
   }
 
-  for(let i = 0; i < lArr.length; i++){
+  for(let i = 0; i < lArr.length; i++){ // converting the strings into numbers
     Number(lArr[i])
     Number(rArr[i])
   }
 
-  let total = 0
+  let total = 0 // summ of the differences
   let multi = 0
 
-  for(let i = 0; i < lArr.length; i++){
-    for(let j = o; j < rArr.length; j++){
-      if(lArr[i] == rArr[j]){
+  for(let i = 0; i < lArr.length; i++){ // checking the occurences of the current index in the adjacent
+    for(let j = 0; j < rArr.length; j++){
+      if(lArr[i] == rArr[j]){ // incrementing the occurences to multiply on the current index
         multi++
       }
-      lArr[i] = lArr[i] * multi
+      lArr[i] *= multi
     }
   }
 
-  for(let el of lArr){
+  for(let el of lArr){ // adding the elements in the left array to a total
     total += el
   }
 
