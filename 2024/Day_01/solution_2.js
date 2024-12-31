7b4665f26d4d8f51b43b7e2cc7713848ea6aa165
JamalColaire <jamal8colaire@gmail.com>
2024-12-31 12:05:57 -0500

    updated comment 2

--- 2024/Day_01/solution_2.js
+++ 2024/Day_01/solution_2.js
@@ -53,6 +53,6 @@ console.log(input) // testing input
 
 let ans = input.split('\n')
 
-console.log(ans) // testing input
+console.log(ans) // testing split input
 
 // console.log(sol2(ans)) // solution
