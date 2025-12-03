0a58850b0eded87dcc094db6c1abce3c1795cbc3
JamalColaire <jamal8colaire@gmail.com>
2025-12-03 09:08:22 -0500

    day 4 solution prep

---
+++ 2025/Day_04/solution_1.js
@@ -0,0 +1,18 @@
+/*
+Day 4 Puzzle-1 solution
+
+
+*/
+
+function sol1(x){
+
+  for(let e of x){}
+}
+
+const fs = require('fs')
+
+const input = fs.readFileSync('./input.txt').toString()
+
+let ans = input
+
+console.log(sol1(ans)) // solution
