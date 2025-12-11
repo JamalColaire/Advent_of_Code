053b425734f0cd6729e211b16d1f816ea890e074
JamalColaire <jamal8colaire@gmail.com>
2025-12-11 09:34:03 -0500

    added solution 2 prep

---
+++ 2025/Day_12/solution_2.js
@@ -0,0 +1,18 @@
+/*
+Day 12 Puzzle-2 solution
+
+
+*/
+
+function sol2(x){
+
+  for(let e of x){}
+}
+
+const fs = require('fs')
+
+const input = fs.readFileSync('./input.txt').toString()
+
+let ans = input
+
+console.log(sol2(ans)) // solution
